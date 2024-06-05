27648be8cd209e2f76bb1183f7ea1d107d39d09d
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-05 09:23:46 +0200

    "242"

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -31,12 +31,38 @@ export class BasicPageComponent implements OnInit{
   constructor( private fb: FormBuilder){}
 
   ngOnInit(): void {
-   this.myForm.reset(rtx5090)
+  //  this.myForm.reset(rtx5090)
 
   }
 
+  getFieldError(field: string): string | null{
+      if(!this.myForm.controls[field] ) return null;
+
+      const errors = this.myForm.controls[field].errors || {};
+
+      for (const key of Object.keys(errors)) {
+        switch(key){
+          case 'required':
+            return 'Este campo es requerido';
+          case 'minlength':
+            return `Mínimo ${ errors['minilength'].requiredLength } caracters`;
+        }
+
+      }
+      return '';
+    }
+
+
+  isValidField( field: string): boolean | null{
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched
+  }
+
   onSave():void {
-    if (this.myForm.valid) return;
+    if (this.myForm.invalid){ this.myForm.markAllAsTouched()
+       return;}
+
+
     console.log(this.myForm.value);
     this.myForm.reset( {price: 10 , inStorage:0});
   }
