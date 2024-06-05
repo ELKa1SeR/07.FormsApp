adf726d347ac6d5f81b01a54769b9d0aa322c80b
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-05 10:59:01 +0200

    "251"

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { waitForAsync } from '@angular/core/testing';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   ]
 })
 export class SwitchesPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required ],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [ false, Validators.requiredTrue],
+  });
+
+  constructor (private fb: FormBuilder ) {}
+
+  onSave(){
+    if (this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+      
+    }
+
+  }
 
 }
