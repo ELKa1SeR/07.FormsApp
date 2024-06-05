0f18b3458f0714622a893007b6c19cac11b80147
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-05 16:04:18 +0200

    "260"

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,43 +1,38 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-//import * as customValidators from '../../../shared/validator/validators'
 import { ValidatorsService } from 'src/app/shared/service/validator.service';
+import { EmailValidator } from 'src/app/shared/validator/email-validator.services';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-
 })
 export class RegisterPageComponent {
 
-  public myForm : FormGroup = this.fb.group({
+  public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
-  })
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2')
+    ]
+  });
 
   constructor(
-    private fb : FormBuilder,
-    public validatorsService: ValidatorsService
-  ){}
+    private fb: FormBuilder,
+    public validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
+  ) { }
 
-  onSubmit(){
-
-      this.myForm.markAllAsTouched();
-
+  onSubmit() {
+    this.myForm.markAllAsTouched();
   }
 
-  isValidField( field: string) {
-
-    return this.validatorsService.isValidField(this.myForm, field)
-
+  isValidField(field: string) {
+    return this.validatorsService.isValidField(this.myForm, field);
   }
-
-
-
-
-
 }
