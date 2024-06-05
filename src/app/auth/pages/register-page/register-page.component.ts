dd4c824566f960dd29d713e8adf137444726a2c6
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-05 12:38:02 +0200

    "253"

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validator/validators';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.css']
+
 })
 export class RegisterPageComponent {
+
+  public myForm : FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  })
+
+  constructor(
+    private fb : FormBuilder
+  ){}
+
+  onSubmit(){
+
+      this.myForm.markAllAsTouched();
+
+  }
+
+  isValidField( field: string) {
+   // TODO obtener validacion desd un servicio
+
+  }
+
+
+
+
 
 }
