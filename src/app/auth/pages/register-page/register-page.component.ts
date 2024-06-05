d4fa5683520ec901fcda9f106dcadf1552285009
FranciscoLiebanas <francisco.liebanas@nter.es>
2024-06-05 12:55:28 +0200

    "255"

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { cantBeStrider } from 'src/app/shared/validator/validators';
+import * as customValidators from '../../../shared/validator/validators'
 
 @Component({
   selector: 'app-register-page',
@@ -10,9 +11,9 @@ import { cantBeStrider } from 'src/app/shared/validator/validators';
 export class RegisterPageComponent {
 
   public myForm : FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   })
